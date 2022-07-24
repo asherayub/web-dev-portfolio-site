7f0215b3f41af94fc4ef22bdfc5b109dabbd9645
Asher Ayub <99917058+asherayub@users.noreply.github.com>
2022-07-24 02:02:46 +0100

    made-responsive

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-let lastScrollTop;
-navbar = document.getElementById('navbar');
-window.addEventListener('scroll',()=>{
-let scrollTop = window.pageYOffset;
-if(scrollTop > lastScrollTop){
-navbar.style.top='-90px';
-}
-else{
-navbar.style.top='0';
-}
-lastScrollTop = scrollTop;
-});
+// let lastScrollTop;
+// if(window.screen.width > 700){
+//     navbar = document.getElementById('navbar');
+// window.addEventListener('scroll',()=>{
+// let scrollTop = window.pageYOffset;
+// if(scrollTop > lastScrollTop){
+// navbar.style.top='-90px';
+// }
+// else{
+// navbar.style.top='0';
+// }
+// lastScrollTop = scrollTop;
+// });
+// }
+
+// for toggler
+let toggleBtn = document.querySelector(".toggler");
+let navBar = document.querySelector(".navbar");
+let navBarItems = document.querySelector(".nav-items");
+let toggleIcon = document.querySelector(".fa-bars");
+toggleBtn.addEventListener("click", () => {
+    navBar.classList.toggle("show-navbar");
+    if (navBar.classList.contains("show-navbar")) {
+        toggleIcon.classList.replace("fa-bars", "fa-xmark");
+    } else {
+        toggleIcon.classList.replace("fa-xmark", "fa-bars");
+    }
+})
